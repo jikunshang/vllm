f22bfd7765d5dff13d3da56475eb28cb3e12920a
Kunshang Ji <kunshang.ji@intel.com>
2024-03-01 00:41:37 +0800

    add xpu

--- vllm/config.py
+++ vllm/config.py
@@ -8,7 +8,7 @@ from transformers import PretrainedConfig
 
 from vllm.logger import init_logger
 from vllm.transformers_utils.config import get_config
-from vllm.utils import get_cpu_memory, is_hip, is_neuron, get_nvcc_cuda_version
+from vllm.utils import get_cpu_memory, is_hip, is_neuron, is_xpu, get_nvcc_cuda_version
 
 logger = init_logger(__name__)
 
@@ -480,6 +480,8 @@ class DeviceConfig:
                 self.device_type = "cuda"
             elif is_neuron():
                 self.device_type = "neuron"
+            elif is_xpu():
+                self.device_type = "xpu"
             else:
                 raise RuntimeError("No supported device detected.")
         else:
