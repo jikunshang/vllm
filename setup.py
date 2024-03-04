f50798d21dccd5d228c4ebf2360bf9fd885ae0c3
Kunshang Ji <kunshang.ji@intel.com>
2024-03-04 20:36:58 +0800

    default build xpu

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from packaging.version import parse, Version
 import setuptools
 import torch
 
-BUILD_XPU_OPS = os.getenv('VLLM_BUILD_XPU_OPS', "0") == "1"
+BUILD_XPU_OPS = os.getenv('VLLM_BUILD_XPU_OPS', "1") == "1"
 
 
 def _is_xpu() -> bool:
