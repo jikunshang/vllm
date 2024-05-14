21b7f30b74c917a093bfa10e25febd6565951bf9
Abhilash Majumder <30946547+abhilash1910@users.noreply.github.com>
2024-05-14 23:23:38 +0530

    add xpu in benchmark_latency.py

--- benchmarks/benchmark_latency.py
+++ benchmarks/benchmark_latency.py
@@ -170,7 +170,7 @@ if __name__ == '__main__':
         "--device",
         type=str,
         default="cuda",
-        choices=["cuda", "cpu"],
+        choices=["cuda", "cpu", "xpu"],
         help='device type for vLLM execution, supporting CUDA and CPU.')
     parser.add_argument('--block-size',
                         type=int,
