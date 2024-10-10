528cce5dd3fe302d5a6a3d41a0107bc47d54e3bf
Kunshang Ji <kunshang.ji@intel.com>
2024-10-10 06:19:06 +0000

    fix

--- vllm/worker/worker_base.py
+++ vllm/worker/worker_base.py
@@ -323,7 +323,7 @@ class LocalOrDistributedWorkerBase(WorkerBase):
                     and self.observability_config.collect_model_execute_time):
                 orig_model_execute_time = intermediate_tensors.tensors.get(
                     "model_execute_time", torch.tensor(0)).item()
-        print(f"model input: {model_input}")
+        # print(f"model input: {model_input}")
         output = self.model_runner.execute_model(
             model_input=model_input,
             kv_caches=self.kv_cache[worker_input.virtual_engine]
