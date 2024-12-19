871823557a9ba45780294c86ab95b9dec2cce025
yan ma <yan.ma@intel.com>
2024-12-19 23:36:05 +0800

    fix lora
    Signed-off-by: yan ma <yan.ma@intel.com>

--- vllm/lora/worker_manager.py
+++ vllm/lora/worker_manager.py
@@ -76,7 +76,6 @@ class WorkerLoRAManager(AbstractWorkerManager):
             lora_config=self.lora_config,
             device=self.device,
             lora_manager_cls=self._manager_cls,
-            device=self.device
         )
         self._adapter_manager = lora_manager
         return lora_manager.model
@@ -206,7 +205,6 @@ class LRUCacheWorkerLoRAManager(WorkerLoRAManager):
             lora_config=self.lora_config,
             device=self.device,
             max_num_batched_tokens=self.max_num_batched_tokens,
-            device=self.device
         )
         self._adapter_manager = lora_manager
         return lora_manager.model
