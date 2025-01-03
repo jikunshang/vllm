80bf86cf726952e65951035d2b5f3a804e0ce78e
Kunshang Ji <kunshang.ji@intel.com>
2025-01-03 14:47:18 +0800

    add ray xpu support

--- vllm/v1/engine/llm_engine.py
+++ vllm/v1/engine/llm_engine.py
@@ -112,8 +112,12 @@ class LLMEngine:
         distributed_executor_backend = (
             vllm_config.parallel_config.distributed_executor_backend)
         if distributed_executor_backend == "ray":
-            from vllm.v1.executor.ray_executor import RayExecutor
-            executor_class = RayExecutor
+            if current_platform.is_cuda():
+                from vllm.v1.executor.ray_executor import RayExecutor
+                executor_class = RayExecutor
+            elif current_platform.is_xpu():
+                from vllm.v1.executor.xpu_ray_executor import RayXPUExecutor
+                executor_class = RayXPUExecutor
         elif distributed_executor_backend == "mp":
             from vllm.v1.executor.multiproc_executor import MultiprocExecutor
             executor_class = MultiprocExecutor
