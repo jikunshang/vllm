d9f16af5221d82e7eb0db3d5949f993aa2946c1d
Kunshang Ji <kunshang.ji@intel.com>
2025-03-08 09:14:53 +0800

    update req

--- examples/offline_inference/basic/basic.py
+++ examples/offline_inference/basic/basic.py
@@ -13,12 +13,16 @@ prompts = [
 sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
 
 # Create an LLM.
-llm = LLM(model="facebook/opt-125m")
-# Generate texts from the prompts. The output is a list of RequestOutput objects
+
+
+    
+if __name__ == "__main__":
+    llm = LLM(model="facebook/opt-125m", dtype="half",  tensor_parallel_size=1, enforce_eager=True)
+    # Generate texts from the prompts. The output is a list of RequestOutput objects
 # that contain the prompt, generated text, and other information.
-outputs = llm.generate(prompts, sampling_params)
-# Print the outputs.
-for output in outputs:
-    prompt = output.prompt
-    generated_text = output.outputs[0].text
-    print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
+    outputs = llm.generate(prompts, sampling_params)
+    # Print the outputs.
+    for output in outputs:
+        prompt = output.prompt
+        generated_text = output.outputs[0].text
+        print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
