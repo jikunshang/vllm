dc350d036856550093f0e675c054a75425be7252
Xinyu Chen <xichen@habana.ai>
2025-03-24 10:42:46 +0200

    Revert "Wrap HPUGraph under forward_context"
    This reverts commit 9c32bbad6a02feacf7d75a3a2ee023187a62a74d.

--- vllm/worker/hpu_model_runner.py
+++ vllm/worker/hpu_model_runner.py
@@ -314,10 +314,7 @@ def modify_decoder_layer(module: torch.nn.Module, suffix="DecoderLayer"):
 class HpuModelAdapter:
 
     def __init__(self, model, vllm_config):
-        if htorch.utils.internal.is_lazy():
-            self.model = htorch.hpu.wrap_in_hpu_graph(model, disable_tensor_cache=True)
-        else:
-            self.model = model
+        self.model = model
         self.prefill_use_fusedsdpa = os.getenv('VLLM_PROMPT_USE_FUSEDSDPA',
                                                '0').lower() in ['1', 'true']
         self.vllm_config = vllm_config
@@ -774,7 +771,8 @@ class HPUModelRunnerBase(ModelRunnerBase[TModelInputForHPU]):
             torch.hpu.synchronize()
 
             with HabanaMemoryProfiler() as m_wrap:
-                self.model = HpuModelAdapter(self.model, vllm_config=self.vllm_config)
+                self.model = _maybe_wrap_in_hpu_graph(
+                    self.model, vllm_config=self.vllm_config)
             msg = f"Wrapping in HPU Graph took {m_wrap.get_summary_string()}"
             logger.info(msg)
 
