b9b82b23109a0570fca185b973a0128e1da2dc9a
yan ma <yan.ma@intel.com>
2025-03-25 21:44:31 +0800

    sliding window support
    Signed-off-by: yan ma <yan.ma@intel.com>

--- vllm/v1/attention/backends/ipex_attn.py
+++ vllm/v1/attention/backends/ipex_attn.py
@@ -52,6 +52,7 @@ class IPEXAttentionBackend(AttentionBackend):
         # TODO: support cascade attention
         return False
 
+
 class IPEXAttentionImpl(AttentionImpl):
 
     def __init__(
@@ -167,6 +168,8 @@ class IPEXAttentionImpl(AttentionImpl):
             0.0,
             self.scale,
             False,
+            self.sliding_window[0],
+            self.sliding_window[1],
             True,
             False,
             None,
