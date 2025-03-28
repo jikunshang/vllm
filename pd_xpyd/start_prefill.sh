4ae8b5771fe527a91f33053b20def89dfb8d4da7
kunshang <kunshang.ji@intel.com>
2025-03-28 10:54:15 +0800

    add disbale async

--- pd_xpyd/start_prefill.sh
+++ pd_xpyd/start_prefill.sh
@@ -9,4 +9,4 @@ export MAX_MODEL_LEN=8192
 # export MODEL_PATH=/software/data/hub/models--facebook--opt-125m/snapshots/27dcfa74d334bc871f3234de431e71c6eeba5dd6/
 export MODEL_PATH=/software/data/models/DeepSeek-R1-BF16-w8afp8-static-no-ste-G2/
 
-MOONCAKE_CONFIG_PATH=./pd_xpyd/mooncake.json python3 -m vllm.entrypoints.openai.api_server --model $MODEL_PATH --port 8100 --max-model-len $MAX_MODEL_LEN --gpu-memory-utilization 0.9 -tp 8  --max-num-seqs 32  --trust-remote-code --kv-transfer-config '{"kv_connector":"MooncakeStoreConnector","kv_role":"kv_producer"}'
+MOONCAKE_CONFIG_PATH=./pd_xpyd/mooncake.json python3 -m vllm.entrypoints.openai.api_server --model $MODEL_PATH --port 8100 --max-model-len $MAX_MODEL_LEN --gpu-memory-utilization 0.9 -tp 8  --max-num-seqs 32  --trust-remote-code --disable-async-output-proc --kv-transfer-config '{"kv_connector":"MooncakeStoreConnector","kv_role":"kv_producer"}'
