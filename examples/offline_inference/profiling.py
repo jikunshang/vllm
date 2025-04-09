9153e61d7c0f778bfc3e6ccd7a041aca28aec697
zh Wang <rekind133@outlook.com>
2025-04-10 01:03:34 +0800

    [Bugfix] Fix profiling.py (#16202)
    Signed-off-by: zh Wang <rekind133@outlook.com>

--- examples/offline_inference/profiling.py
+++ examples/offline_inference/profiling.py
@@ -235,9 +235,8 @@ def run_profile(context: ProfileContext, csv_output: Optional[str],
             sampling_params.max_tokens = next(output_len_generator)
             assert isinstance(sampling_params.max_tokens, int)
 
-            prompt_token_ids = torch.randint(
-                min(llm.llm_engine.model_config.get_vocab_size(), llm.llm_engine.model_config.max_model_len),
-                size=(prompt_len, )).tolist()
+            prompt_token_ids = torch.randint(llm.get_tokenizer().vocab_size,
+                                             size=(prompt_len, )).tolist()
 
             llm.llm_engine.add_request(
                 request_id=f"seq{i}",
