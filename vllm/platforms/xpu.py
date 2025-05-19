71daf02df0cd586162ada048c48801beaa125731
Yejing Lai <yejing.lai@intel.com>
2025-05-19 13:29:41 +0800

    Fix block_size setting hard code; (#175)

--- vllm/platforms/xpu.py
+++ vllm/platforms/xpu.py
@@ -71,7 +71,7 @@ class XPUPlatform(Platform):
     def check_and_update_config(cls, vllm_config: VllmConfig) -> None:
         cache_config = vllm_config.cache_config
         # in V1(or with ipex chunked prefill) block_size is 64
-        if cache_config and envs.VLLM_USE_V1:
+        if cache_config and cache_config.block_size is None and envs.VLLM_USE_V1:
             cache_config.block_size = 64
         if cache_config and cache_config.block_size is None:
             cache_config.block_size = 16
