cf19f81d4758b6541035da5f9d070d49c43340bb
Yan Ma <yan.ma@intel.com>
2025-06-26 14:14:48 +0800

    offload weights to cpu before fp8 online quant (#225)
    Signed-off-by: yan <yanma1@habana.ai>

--- vllm/envs.py
+++ vllm/envs.py
@@ -119,6 +119,7 @@ if TYPE_CHECKING:
     VLLM_ALL2ALL_BACKEND: str = "naive"
     VLLM_MAX_TOKENS_PER_EXPERT_FP4_MOE: int = 163840
     VLLM_XPU_FP8_DTYPE: str = "e5m2"
+    VLLM_OFFLOAD_WEIGHTS_BEFORE_QUANT: bool = False
 
 
 def get_default_cache_root():
@@ -829,6 +830,10 @@ environment_variables: dict[str, Callable[[], Any]] = {
     # fp8 dtype for XPU platform
     "VLLM_XPU_FP8_DTYPE":
     lambda: os.environ.get("VLLM_XPU_FP8_DTYPE", "e5m2"),
+
+    # Offload model weights to cpu before online fp8 quantization
+    "VLLM_OFFLOAD_WEIGHTS_BEFORE_QUANT":
+    lambda: os.environ.get("VLLM_OFFLOAD_WEIGHTS_BEFORE_QUANT", "0") == "1",
 }
 
 # --8<-- [end:env-vars-definition]
