77e3cad4913ed603136979c1ae220af8396d041c
Xinyu Chen <xichen@habana.ai>
2025-08-06 14:34:47 +0800

    [DeepSeek R1] Prefill DP under PD Dis mode (#1627)

--- vllm/worker/hpu_model_runner.py
+++ vllm/worker/hpu_model_runner.py
@@ -967,8 +967,7 @@ class HPUModelRunnerBase(ModelRunnerBase[TModelInputForHPU]):
         real_batch_size = len(seq_group_metadata_list)
         batch_size_padded = self.bucketing_ctx.get_padded_batch_size(
             real_batch_size, is_prompt)
-        if self.dp_awared_padding and (self.vllm_config.kv_transfer_config
-                                       is None or not is_prompt):
+        if self.dp_awared_padding:
             if self.is_driver_worker:
                 batch_size_padded = align_dp_groups(
                     batch_size_padded, torch.distributed.ReduceOp.MAX)
@@ -1193,10 +1192,7 @@ class HPUModelRunnerBase(ModelRunnerBase[TModelInputForHPU]):
             self.bucketing_ctx.get_padded_prompt_seq_len(max_query_len),
             self.block_size)
 
-        # Align the prompt length across DP groups when PD aggregation is
-        # not enabled.
-        if self.dp_awared_padding and \
-            self.vllm_config.kv_transfer_config is None:
+        if self.dp_awared_padding:
             if self.is_driver_worker:
                 max_prompt_len = align_dp_groups(
                     max_prompt_len, torch.distributed.ReduceOp.MAX)
@@ -1914,7 +1910,11 @@ class HPUModelRunnerBase(ModelRunnerBase[TModelInputForHPU]):
 
     def _dummy_run(self, max_num_batched_tokens: int) -> None:
         assert max_num_batched_tokens == 1
-        self.warmup_scenario(max_num_batched_tokens, 1, False, None, False,
+        is_prompt = False
+        if self.vllm_config.kv_transfer_config is not None and \
+            self.vllm_config.kv_transfer_config.is_kv_producer:
+            is_prompt = True
+        self.warmup_scenario(max_num_batched_tokens, 1, is_prompt, None, False,
                              True, False, 0, 1, True, True)
         return
 
