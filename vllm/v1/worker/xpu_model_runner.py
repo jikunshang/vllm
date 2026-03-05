bbaa1a735e2bf22a8ce53cd53e5040ce49d27972
Xinyu Chen <xinyu1.chen@intel.com>
2026-03-06 01:19:18 +0800

    [XPU] Enable ModelRunnerV2 on XPU (#36078)
    Signed-off-by: Xinyu Chen <xinyu1.chen@intel.com>
    
    Signed-off-by: Kunshang Ji <kunshang.ji@intel.com>

--- vllm/v1/worker/xpu_model_runner.py
+++ vllm/v1/worker/xpu_model_runner.py
@@ -8,6 +8,9 @@ import torch
 from vllm.config import VllmConfig
 from vllm.logger import init_logger
 from vllm.utils.torch_utils import supports_xpu_graph
+from vllm.v1.worker.gpu.model_runner import (
+    GPUModelRunner as GPUModelRunnerV2,
+)
 from vllm.v1.worker.gpu_model_runner import GPUModelRunner
 
 if TYPE_CHECKING:
@@ -29,6 +32,18 @@ class XPUModelRunner(GPUModelRunner):
         # FIXME: To be verified.
         self.cascade_attn_enabled = False
 
+
+class XPUModelRunnerV2(GPUModelRunnerV2):
+    """A model runner for XPU devices."""
+
+    def __init__(
+        self,
+        vllm_config: VllmConfig,
+        device: torch.device,
+    ):
+        with _torch_cuda_wrapper():
+            super().__init__(vllm_config, device)
+
     def _sync_device(self) -> None:
         torch.xpu.synchronize()
 
@@ -42,10 +57,13 @@ def _torch_cuda_wrapper():
         torch.cuda.current_stream = torch.xpu.current_stream
         torch.cuda.stream = torch.xpu.stream
         torch.cuda.mem_get_info = torch.xpu.mem_get_info
+        torch.cuda.Event = torch.Event
+        torch.cuda.set_stream = torch.xpu.set_stream
         torch.cuda.synchronize = torch.xpu.synchronize
         if supports_xpu_graph():
             torch.cuda.graph = torch.xpu.graph
             torch.cuda.CUDAGraph = torch.xpu.XPUGraph
+            torch.cuda.graph_pool_handle = torch.xpu.graph_pool_handle
             torch.cuda.empty_cache = torch.xpu.empty_cache
         yield
     finally:
